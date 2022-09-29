67e4d5a7042f211f78b296ec4059d14958b27e5d
matucasal <matias.casal@yahoo.com.ar>
2022-09-29 10:36:08 -0300

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+}
