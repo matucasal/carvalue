6c8ff9afde2a083b72d85142b9a924b8e4fe512a
matucasal <matias.casal@yahoo.com.ar>
2022-09-29 12:09:24 -0300

    adding interceptos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,16 @@ import {
   Param,
   Query,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -22,8 +26,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
   }
 
   @Get()
@@ -32,7 +41,7 @@ export class UsersController {
   }
 
   @Delete('/:id')
-  removeUser(@Param('id') id: string){
+  removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
 
