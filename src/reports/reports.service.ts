1279addf2abd6c1622d49613758796011298d6ba
matucasal <matias.casal@yahoo.com.ar>
2022-10-03 10:24:30 -0300

    changin get authuser to middleware

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
@@ -9,9 +9,18 @@ import { User } from '../users/user.entity';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto, user: User){
+  create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
     report.user = user;
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: number, approved: boolean) {
+    const report = await this.repo.findOneBy({ id });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+    report.approved = approved;
     return this.repo.save(report);
   }
 }
