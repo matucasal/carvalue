d36da77ba0bb3f79f22f691151e779fe240c946c
matucasal <matias.casal@yahoo.com.ar>
2022-10-03 06:34:32 -0300

    adding test and e2e test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication system (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'asda1@fasfa.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: '43434f' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+
+  it('signup as new user then get the current logg in user', async () => {
+    const email = 'tests@tete.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '34353' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
+
+  
+});
